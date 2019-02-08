eabaf00d714f7dc2fb2c226e87153011d72192e4
Sabiyeva Akmaral <akmaral.150400@gmail.com>
2019-02-08 17:00:21 +0600

    Tasks LAB1

---
+++ W1LAB1/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "[*]"; // один пример треугольника
+            int a = int.Parse(Console.ReadLine()); // Ввод числа
+            for (int i = 1; i<= a; i++) //используем loop в loop
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(s); //вывод возрастающей каждой итерацией треугольника
+
+                }
+                Console.WriteLine(); // переход в следующую строку
+            }
+            Console.ReadLine(); // чтобы окно консоль не закрывался
+        }
+    }
+}
